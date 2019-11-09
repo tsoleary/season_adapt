68eec6f0095cc111834713418c2d7ed6056d08c0
tsoleary <tsoleary12@gmail.com>
2019-11-09 14:09:59 -0500

    init functions.R

---
+++ functions.R
@@ -0,0 +1,55 @@
+# functions for seasonal adaptation model --------------------------------------
+
+# Function to initialise population --------------------------------------------
+init_pop <- function(L, pop_size) {
+  # initialize diploid chromosomes with a random population with equal allele freq
+  genome <- matrix(sample(0:1, 2*L*pop_size, replace = TRUE, prob = c(0.5, 0.5)), 
+                   nrow = pop_size*2, ncol = L)
+  loci <- paste("locus", str_pad(1:L, 2, "0", side = "left"), sep = "_")
+  colnames(genome) <- loci
+  # name the individuals and their chromosome pairs for dplyr later
+  individual <- paste("indiv", 
+                      str_pad(rep(1:pop_size, each = 2), 6, "0", side = "left"), 
+                      sep = "_")
+  chr <- paste("chr", rep(1:2, times = pop_size), sep = "_")
+  # bind the matrix and names together
+  genome <- as.tibble(cbind(individual, chr, genome))
+  return(genome)
+}
+
+# Funtion to calculate fitness -------------------------------------------------
+fitness_func <- function(genomes) {
+  # long format for dplyr
+  gen_tidy <- genomes %>%
+    pivot_longer(cols = contains("locus"), 
+                 names_to = "loci", 
+                 values_to = "allele")
+  # sumarize each locus for each inidividual across the two chromosomes
+  genotype <- gen_tidy %>%
+    group_by(individual, loci) %>%
+    summarize(geno = sum(as.numeric(allele)))
+  
+  # count of each genotype, genotype (0,1 or 2) for each loci in an individual
+  geno_count <- genotype %>%
+    group_by(individual, geno) %>%
+    count()
+  # seasonal score for each individual
+  z_score <- geno_count %>%
+    pivot_wider(names_from = geno, values_from = n) %>%
+    rename("geno_0" = "0", "geno_1" = "1", "geno_2" = "2") %>%
+    mutate(z_winter = geno_0 + geno_1 * d,
+           z_summer = geno_2 + geno_1 * d)
+  # fitness of each individual, based on equation in paper
+  fitness <- z_score %>%
+    mutate(f_winter = (1 + z_winter) ^ y,
+           f_summer = (1 + z_summer) ^ y)
+  return(fitness)
+}
+
+# # Selecting individuals for the next generation, depends on season
+# curr_season <- season
+# if (curr_season == "summer") { # select 2 parents at random weighted by fitness
+#   selected_idx <- sample(seq_len(nrow(fitness)), 2, prob=fitness$f_summer, replace = FALSE)
+# } else {
+#   selected_idx <- sample(seq_len(nrow(fitness)), 2, prob=fitness$f_winter, replace = FALSE)  
+# }
