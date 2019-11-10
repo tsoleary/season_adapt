f8eb3c0e605ddb15f1b8451b45f498fe263c0c9d
tsoleary <tsoleary12@gmail.com>
2019-11-10 17:54:08 -0500

    commented mutate_genome function and made it so it is no longer dependent on the .GlobalEnv variables L and pop_size

--- functions.R
+++ functions.R
@@ -2,52 +2,57 @@
 
 # Function to initialise population --------------------------------------------
 init_pop <- function(L, pop_size) {
-  # initialize diploid chromosomes with a random population with equal allele freq
-  genome <- matrix(sample(0:1, 2*L*pop_size, replace = TRUE, prob = c(0.5, 0.5)), 
-                   nrow = pop_size*2, ncol = L)
-  loci <- paste("locus", str_pad(1:L, 2, "0", side = "left"), sep = "_")
-  colnames(genome) <- loci
-  # name the individuals and their chromosome pairs for dplyr later
+  # initialize diploid chromosomes with a random population
+  genome <- matrix(sample(0:1, 2*L*pop_size, 
+                          replace = TRUE, 
+                          prob = c(0.5, 0.5)), 
+                   nrow = pop_size * 2, 
+                   ncol = L)
+  
+  # name the loci, the individuals, and their chromosome pairs
+  colnames(genome) <- paste("locus", 
+                            str_pad(1:L, 2, "0", side = "left"), sep = "_")
   individual <- paste("indiv", 
                       str_pad(rep(1:pop_size, each = 2), 6, "0", side = "left"), 
                       sep = "_")
   chr <- paste("chr", rep(1:2, times = pop_size), sep = "_")
+  
   # bind the matrix and names together
-  genome <- as.tibble(cbind(individual, chr, genome))
-  return(genome)
+  return(as_tibble(cbind(individual, chr, genome)))
 }
 
 
 # Funtion to calculate fitness -------------------------------------------------
 fitness_func <- function(genomes) {
-  # long format for dplyr
-  gen_tidy <- genomes %>%
+  # creates the diploid genotypes: 0 (homo 00), 1 (het), or 2 (homo 11)
+  genotype <- genomes %>%
     pivot_longer(cols = contains("locus"), 
                  names_to = "loci", 
-                 values_to = "allele")
-  # sumarize each locus for each inidividual across the two chromosomes=
-  genotype <- gen_tidy %>%
+                 values_to = "allele") %>%
     group_by(individual, loci) %>%
     summarize(geno = sum(as.numeric(allele)))
   
-  # count of each genotype, genotype (0,1 or 2) for each loci in an individual
+  # count of each genotype for each loci in an individual
   geno_count <- genotype %>%
     group_by(individual, geno) %>%
     count()
+  
   # seasonal score for each individual
   z_score <- geno_count %>%
     pivot_wider(names_from = geno, values_from = n) %>%
     rename("geno_0" = "0", "geno_1" = "1", "geno_2" = "2") %>%
     mutate(z_winter = geno_0 + geno_1 * d,
            z_summer = geno_2 + geno_1 * d)
-  # fitness of each individual, based on equation in paper
+  
+  # fitness of each individual
   fitness <- z_score %>%
     mutate(f_winter = (1 + z_winter) ^ y,
            f_summer = (1 + z_summer) ^ y)
+  
   return(fitness)
 }
 
-# Selecting individuals for the next generation, depends on season
+# Selecting individuals for the next generation, depends on season -------------
 
 select_inds <- function(genomes,fitness_all, season) {
   if (season == "summer") { # select 2 parents at random weighted by fitness
@@ -74,33 +79,45 @@ cross_over <- function(ind, cross_prob) {
     cross_genome[c(1,2), ] <- cross_genome[c(2,1), ]
     stripped[,cross_loc:L] <- cross_genome
   }
-  return(cbind(strip_names, stripped))
+  return(as_tibble(cbind(strip_names, stripped)))
 }
 
 # mutation on the entire population each generation before crossover -----------
-mutate_genome <- function(genomes,mut_prob) {
-  mut_test_mat <- matrix(runif(L*pop_size*2), 
-                         nrow = pop_size * 2, 
-                         ncol = L)
-  mut_pos <- mut_test_mat < mut_prob
+mutate_genome <- function(genomes, mut_prob) {
+  # multiply the mutation probabilty by two (because half the time 0's will 
+  # be mutated to 0's and same for 1's)
+  mut_prob_2 <- mut_prob * 2
   
-  mut_vals <- matrix(sample(0:1, 50, replace = TRUE), 
-                     nrow = pop_size * 2, 
-                     ncol = L)
+  # total number of chromosomes and loci in the genome
+  total_chr <- nrow(genomes)
+  genome_length <- sum(grepl("locus", colnames(genomes)))
   
-  mut_mat <- mut_vals * mut_pos
+  # determine mutation positions with a random uniform distribution
+  mut_pos <- matrix(runif(genome_length * total_chr), 
+                         nrow = total_chr, 
+                         ncol = genome_length) < mut_prob_2
   
-  mat <- matrix(as.numeric(as.matrix(select(genomes, contains("locus")))),
-                nrow = pop_size * 2,
-                ncol = L)
+  # mutation values (with zeros where the original values are located)
+  mut_mat <- matrix(sample(0:1, genome_length, replace = TRUE), 
+                     nrow = total_chr, 
+                     ncol = genome_length) * mut_pos
+  
+  # original values (with zeros where the mutated values are located)
+  org_mat <- matrix(as.numeric(as.matrix(select(genomes, contains("locus")))),
+                nrow = total_chr, 
+                ncol = genome_length) * !mut_pos
 
-  par_mat <- mat * !mut_pos
+  # combine matrices by addition
+  new_mat <- mut_mat + org_mat
   
-  new_mat <- mut_mat + par_mat
+  # add indiv and chr identifiers to dataframe
   strip_names <- select(genomes, -contains("locus"))
   df <- cbind(strip_names, new_mat)
+  
+  # keep column names the same for the loci
   colnames(df) <- colnames(genomes)
-  return(df)
+  
+  return(as_tibble(df))
 }
 
 
