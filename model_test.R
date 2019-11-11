f79e2027212a574902aedfe156e03a78eba86201
cpetak <csenge.petak@uvm.edu>
2019-11-11 11:20:38 -0500

    first experiment working! when only selecting for summer genotype number of alleles for summer is increasing over time!

--- model_test.R
+++ model_test.R
@@ -34,25 +34,37 @@ genomes <- init_pop(L, pop_size) # initial population
 fitness_all <- fitness_func(genomes) # fitness at time 0
 season <- "summer" # initial season
 
-
-mutated_genome <- mutate_genome(genomes,mut_prob) 
-
-
 # Start experiment -------------------------------------------------------------
+start_genomes <- select(genomes, contains("locus"))
+start_genomes <-  data.matrix(start_genomes, rownames.force = NA) #saving for comparing later
+sum(start_genomes)
+
 for (year in 1:years){
   for (generation in 1:generations){
+    new_pop <- init_pop(L, pop_size)
     for (i in 1:pop_size){
       selected_for_mating <- select_inds(genomes, fitness_all, season)
       crossed1<-cross_over(selected_for_mating[[1]], cross_prob)
       new_ind_chr1 <- crossed1[sample(seq_len(nrow(crossed1)), 1),]
       crossed2 <- cross_over(selected_for_mating[[2]], cross_prob)
       new_ind_chr2 <- crossed2[sample(seq_len(nrow(crossed2)), 1),]
-      new_ind_name <- i
+      loci1 <- select(new_ind_chr1, contains("locus"))
+      loci2 <- select(new_ind_chr2, contains("locus"))
+      new_pop[2*i,3:L] <- loci1 #this is not ideal, don't know how to improve
+      new_pop[(2*i)-1,3:L] <- loci2
     }
+    genomes <- new_pop
+    #genomes <- mutate_genome(genomes,mut_prob) 
     print("one generation has passed")
   }
   print("one year has passed")
 }
+
+end_genomes <- select(genomes, contains("locus"))
+end_genomes <-  data.matrix(end_genomes, rownames.force = NA) #saving for comparing later
+sum(end_genomes)
+
+
 
 
 
