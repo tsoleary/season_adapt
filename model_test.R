d00bbb14c2e33707aec4bb01a97a61b6bf7b24d2
cpetak <csenge.petak@uvm.edu>
2019-11-11 17:35:52 -0500

    fixed bug which was not updating fitness for every new generation

--- model_test.R
+++ model_test.R
@@ -9,26 +9,25 @@ source("functions.R")
 
 # Parameters -------------------------------------------------------------------
 # number of loci in a genome
-L <- 50
+L <- 100
 # population size 
-pop_size <- 10
+pop_size <- 100
 # dominance
 d <- 0.5
 # exponent of the fitness function? epistasis parameter or something?
 y <- 1
 # recombination rate
-cross_prob <- 0.01
+cross_prob <- 0
 # mutation rate
 mut_prob <- 0.05 #2*10^(-4)
 # duration of experiment in years
-years <- 10
+years <- 1
 # number of generations in a season
-generations <- 20
+generations <- 50
 # balance between seasons (2 is even, less than 2 means more summer, etc.)
 seasonal_balance <- 2
 
-#site in genome = 0 = summer, 1 = winter
-#time of the year = 0 = summer, 1 = winter
+#site in genome = 1 = summer, 0 = winter
 
 # Initialize Population --------------------------------------------------------
 
@@ -45,19 +44,17 @@ genomes_over_time[1]<-sum(new_overall_genome)
 
 for (year in 1:years){
   for (generation in 1:generations){
-    if (generation < generations / seasonal_balance){
-      season <- "summer"
-    } else {
-      season <- "winter"
-    }
+    # if (generation < generations / seasonal_balance){
+    #   season <- "summer"
+    # } else {
+    #   season <- "winter"
+    # }
+    new_pop <- init_pop(L, pop_size) #prob_0 = 1, prob_1 = 0)
     for (i in 1:pop_size){
-      # 
-      new_pop <- init_pop(L, pop_size, prob_0 = 1, prob_1 = 0)
-      
       selected_for_mating <- select_inds(genomes, fitness_all, season)
+      
       crossed1 <- cross_over(selected_for_mating[[1]], cross_prob)
       new_ind_chr1 <- crossed1[sample(seq_len(nrow(crossed1)), 1),]
-      
       crossed2 <- cross_over(selected_for_mating[[2]], cross_prob)
       new_ind_chr2 <- crossed2[sample(seq_len(nrow(crossed2)), 1),]
       
@@ -66,12 +63,12 @@ for (year in 1:years){
       
       new_pop[2*i,3:L] <- loci1 #this is not ideal, don't know how to improve
       new_pop[(2*i)-1,3:L] <- loci2
-      
     }
     genomes <- new_pop
     new_overall_genome <- select(genomes, contains("locus"))
     new_overall_genome <-  data.matrix(new_overall_genome, rownames.force = NA) #saving for comparing later
     genomes_over_time[generation]<-sum(new_overall_genome)
+    fitness_all <- fitness_func(genomes)
     #genomes <- mutate_genome(genomes,mut_prob) 
     print(paste("year", year, "generation", generation))
   }
