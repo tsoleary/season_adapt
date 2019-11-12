f6740ce84a20f6bd6c5ec50495d1d5bc2c2582f0
tsoleary <tsoleary12@gmail.com>
2019-11-12 11:20:12 -0500

    few super minor changes and testing out stuff and timing

--- model_test.R
+++ model_test.R
@@ -14,33 +14,33 @@ L <- 50
 pop_size <- 100
 # dominance
 d <- 0.5
-# exponent of the fitness function? epistasis parameter or something?
+# exponent of the fitness function
 y <- 1
 # recombination rate
 cross_prob <- 0.03
 # mutation rate
-mut_prob <- 0.05 #2*10^(-4)
+mut_prob <- 1*10^(-4)
 # duration of experiment in years
-years <- 5
+years <- 20
 # number of generations in a season
-generations <- 50
+generations <- 20
 # balance between seasons (2 is even, less than 2 means more summer, etc.)
 seasonal_balance <- 2
 
 #site in genome = 1 = summer, 0 = winter
 
+start <- proc.time()
+
 # Initialize Population --------------------------------------------------------
 
-genomes <- init_pop(L, pop_size) # initial population
-fitness_all <- fitness_func(genomes) # fitness at time 0
-season <- "summer" # initial season
+genomes <- init_pop(L, pop_size) 
 
 # Start experiment -------------------------------------------------------------
 genomes_over_time <- vector()
 
 new_overall_genome <- select(genomes, contains("locus"))
-new_overall_genome <-  data.matrix(new_overall_genome, rownames.force = NA) #saving for comparing later
-genomes_over_time[1]<-sum(new_overall_genome)
+new_overall_genome <-  data.matrix(new_overall_genome, rownames.force = NA)
+genomes_over_time[1] <- sum(new_overall_genome)
 
 G <- 1
 
@@ -51,10 +51,13 @@ for (year in 1:years){
     } else {
       season <- "winter"
     }
-    new_pop <- init_pop(L, pop_size) #prob_0 = 1, prob_1 = 0)
+    # create an empty population data frame with all zeros
+    new_pop <- init_pop(L, pop_size, prob_0 = 1, prob_1 = 0)
     for (i in 1:pop_size){
+      # select parents based on fitness
+      fitness_all <- fitness_func(genomes)
       selected_for_mating <- select_inds(genomes, fitness_all, season)
-      
+      # cross chromosomes of parents
       crossed1 <- cross_over(selected_for_mating[[1]], cross_prob)
       new_ind_chr1 <- crossed1[sample(seq_len(nrow(crossed1)), 1),]
       crossed2 <- cross_over(selected_for_mating[[2]], cross_prob)
@@ -63,24 +66,31 @@ for (year in 1:years){
       loci1 <- select(new_ind_chr1, contains("locus"))
       loci2 <- select(new_ind_chr2, contains("locus"))
       
-      new_pop[2*i,3:L] <- loci1 #this is not ideal, don't know how to improve
-      new_pop[(2*i)-1,3:L] <- loci2
+      new_pop[2*i, which(grepl("locus", colnames(new_pop)))] <- loci1 
+      new_pop[(2*i)-1, which(grepl("locus", colnames(new_pop)))] <- loci2
     }
     genomes <- new_pop
+    
+    # get a sum of all 1 alleles in the population for each generation
     new_overall_genome <- select(genomes, contains("locus"))
-    new_overall_genome <-  data.matrix(new_overall_genome, rownames.force = NA) #saving for comparing later
+    new_overall_genome <-  data.matrix(new_overall_genome, rownames.force = NA) 
     genomes_over_time[G]<-sum(new_overall_genome)
-    fitness_all <- fitness_func(genomes)
-    genomes <- mutate_genome(genomes,mut_prob) 
-    print(paste("year", year, "generation", generation))
-    print(season)
+
+    
+    # mutate genomes for the next year
+    genomes <- mutate_genome(genomes, mut_prob)
+    
+    # print information to keep track of simulation progress
+    print(paste("year", year, "generation", generation, "season", season,
+                "total generation", G))
     G <- G + 1
-    print(G)
   }
 }
 
+time_elapsed <- proc.time() - start
 
-plot(genomes_over_time, ylab="Number of allele 1 in population", xlab="Number of generations")
+plot(genomes_over_time, ylab = "Number of allele 1 in population", 
+     xlab = "Number of generations")
 lines(genomes_over_time)
 
 
