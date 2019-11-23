fd4ecd32fa0c1f3da8e0ba8ea185e056fcbb0f76
cpetak <csenge.petak@uvm.edu>
2019-11-23 14:05:50 -0500

    set default parameters

--- model_test.R
+++ model_test.R
@@ -23,23 +23,23 @@ source("functions.R")
 
 # Run a simulation -------------------------------------------------------------
 tictoc::tic()
-numreps <- 2
-d <- c(0.2, 0.5, 0.8)
-num_generations <- c(10,20,50)
-cross_over_nums <- c(0.01, 0.05, 0.1)
-pops <- c(100,1000,10000)
-y <- c(0.5,1.0,2.0)
-sesss <- c(1.25,1.5,2.0)
+numreps <- 20
+d <- c(0.2, 0.5, 0.8) #Tom
+num_generations <- c(10,20,50) #Tom
+cross_over_nums <- c(0.01, 0.05, 0.1, 0.5) #Csenge
+pops <- c(100,1000,10000) #Alison
+y <- c(0.5,1.0,2.0) #Alison
+seas_bal <- c(1.25,1.5,2.0) #Csenge
 
 for (k in d){
   for (i in 1:numreps){
-    run_simulation(L = 50, 
-                   pop_size = 100,
-                   d = k,
+    run_simulation(L = 250, 
+                   pop_size = 1000,
+                   d = 0.5,
                    y = 1, 
-                   cross_prob = 0.03,
+                   cross_prob = 0.05,
                    mut_prob = 1*10^(-4),
-                   years = 1,
+                   years = 300,
                    generations = 20,
                    seasonal_balance = 2,
                    rep = i)
@@ -47,4 +47,17 @@ for (k in d){
   }
 }
 tictoc::toc()
+
+#default parameters
+# 
+# (L = 250, 
+#   pop_size = 1000,
+#   d = 0.5,
+#   y = 1, 
+#   cross_prob = 0.05,
+#   mut_prob = 1*10^(-4),
+#   years = 300,
+#   generations = 20,
+#   seasonal_balance = 2,
+#   rep = i)
 
