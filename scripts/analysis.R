6611e54af2b67bc3cac7f3429d22d1031db14394
tsoleary <tsoleary12@gmail.com>
2019-11-26 10:44:41 -0500

    proportion of loci at fixation plot added

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -20,8 +20,8 @@ param <- str_split(names, "_", simplify = TRUE)[,c(3,5,7,9,11,13,15,18)]
 caption <- paste(paste("Generations per year", param[rep, 1]),
                 paste("Pop size", param[rep, 2]),
                 paste("Seasonal Balance", param[rep, 3]),
-                paste("Number of Loci", paste0(param[rep, 4], "\n")),
-                paste("Dominance", param[rep, 5]),
+                paste("Number of Loci", param[rep, 4]),
+                paste("\nDominance", param[rep, 5]),
                 paste("Epistasis", param[rep, 6]),
                 paste("Crossover probability", param[rep, 7]),
                 paste("Replicate number", param[rep, 8]), sep = "; ")
@@ -35,10 +35,30 @@ sim_results %>%
   filter(genz < 2000) %>%
   plot_freq(figure_caption = caption, fig_title = value)
 
-# plot results between genz 5000 and 6000
+# plot results less than generation 2000 and divisible by 10 to get only the 
+# freq at the seasonal turns for gen/year = 20
 sim_results %>%
-  filter(genz > 5000 & genz < 6000) %>%
+  filter(genz < 2000 & genz %% 10 == 0) %>%
   plot_freq(figure_caption = caption, fig_title = value)
+
+# plot results between genz 500 and 600
+sim_results %>%
+  filter(genz > 500 & genz < 600) %>%
+  plot_freq(figure_caption = caption, fig_title = value)
+
+
+# plot the proportion of loci at fixation
+sim_results %>%
+  filter(genz < 6000) %>%
+  group_by(genz) %>%
+  filter(freqs == 0 | freqs == 1) %>%
+  count() %>%
+  mutate(freqs = n / as.numeric(param[rep, 4])) %>%
+  ggplot(mapping = aes(x = genz, y = freqs)) +
+  geom_line() + 
+  ylim(0, 1) +
+  ylab("Propotion of fixed loci") +
+  xlab("Generations")
 
 
 
