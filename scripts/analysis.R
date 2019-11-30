95e8c44932616cef017e28ba59d72c303b484f24
tsoleary <tsoleary12@gmail.com>
2019-11-29 22:21:48 -0500

    do_analysis moved. and density plots created. messy!

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -6,7 +6,7 @@ require(rlist)
 source(here::here("functions.R"))
 
 # set working directory to the results folder of interest
-setwd(here::here("results/generation"))
+setwd(here::here("results/generations"))
 
 # get a list of the files that match the variable value of interest
 value <- "G_50"
@@ -80,88 +80,57 @@ do_analysis(values) #first boxplot = means, second boxplot = standard deviation,
                     #in the order you put it in the values variable above
                     #also gives you 3 ANOVA: first means, second standard deviation, third ratio of fixed loci
 
-#first run function below, then call it
-do_analysis <- function(values){
-  
-  files = c()
-  for (value in values){
-    file <- list.files()[grep(value, list.files())]
-    files <- list.append(files, file) #contains all the files regarding your parameter in the correct order
-  }
-  
-  sim_results <- vector(mode = "list", length = 15)
-  i = 1
-  for (f in files){
-    sim_result <- read.csv(f)
-    sim_results[i] <- list(sim_result) #contains all dataframes of your parameter (length = 15)
-    i <- i+1
-  }
-  
-  maximums <- c()
-  for (rep in sim_results){
-    maximums <- list.append(maximums, max(rep$genz)) #this ensures that the minimum of all the maximum generations is used for analysis
-  }
-    
-  means <- c()
-  standard_devs <- c()
-  for (rep in sim_results){
-    temp <- rep[rep$genz == min(maximums),] #check that all have same max generation!
-    means <- list.append(means, mean(temp$freqs))
-    standard_devs <- list.append(standard_devs, sd(temp$freqs))
-  }
-  
-  means1 <- means[1:5] #means of loci frequency when parameter = first input value
-  means2 <- means[6:10] #means of loci frequency when parameter = second input value
-  means3 <- means[11:15] #means of loci frequency when parameter = third input value
-  
-  x <- data.frame("Categories" = c("Means1","Means1","Means1","Means1","Means1","Means2","Means2","Means2","Means2","Means2","Means3","Means3","Means3","Means3","Means3"), "Means" = c(means1,means2,means3))
-  
-  ANOVAofmeans <- aov(x$Means ~ x$Categories)
-  print(summary(ANOVAofmeans))
-  
-  boxplot(x$Means ~ x$Categories,
-          ylab="Mean loci frequency",
-          xlab= "Dominance values", #REPLACE THIS
-          names=c("d = 0.2","d = 0.5","d = 0.8"), #REPLACE THIS
-          col= c("gray", "blue", "green")
-  )
-  
-  sd1 <- standard_devs[1:5] 
-  sd2 <- standard_devs[6:10] 
-  sd3 <- standard_devs[11:15]
-  
-  y <- data.frame("Categories" = c("SD1","SD1","SD1","SD1","SD1","SD2","SD2","SD2","SD2","SD2","SD3","SD3","SD3","SD3","SD3"), "SDs" = c(sd1,sd2,sd3))
-  
-  ANOVAofsds <- aov(y$SDs ~ y$Categories)
-  print(summary(ANOVAofsds))
-  
-  boxplot(y$SDs ~ y$Categories,
-          ylab="Standard deviation of loci frequencies",
-          xlab= "Dominance values",
-          names=c("d = 0.2","d = 0.5","d = 0.8"),
-          col= c("gray", "blue", "green")
-  ) 
-  
-  fixed <- c()
-  for (rep in sim_results){
-    temp <- rep[rep$genz == min(maximums),] #check that all have same max!
-    temp <- round(temp$freqs, digits = 1) #round to 1 digit so that 0.99 would be considered fixed
-    fixed <- list.append(fixed, length(which(temp==0 | temp == 1))/length(temp))
-  }
-  
-  fix1 <- fixed[1:5]
-  fix2 <- fixed[6:10]
-  fix3 <- fixed[11:15]
-  
-  z <- data.frame("Categories" = c("fix1","fix1","fix1","fix1","fix1","fix2","fix2","fix2","fix2","fix2","fix3","fix3","fix3","fix3","fix3"), "Fixs" = c(fix1,fix2,fix3))
-  
-  ANOVAoffixs <- aov(z$Fixs ~ z$Categories)
-  print(summary(ANOVAoffixs))
-  
-  boxplot(z$Fixs ~ z$Categories,
-          ylab="Proportion of fixed loci",
-          xlab= "Dominance values",
-          names=c("d = 0.2","d = 0.5","d = 0.8"),
-          col= c("gray", "blue", "green")
-  ) 
+
+
+
+# histogram and density plots --------------------------------------------------
+
+# set working directory to the results folder of interest
+setwd(here::here("results/dominance"))
+
+# filter only the final generation
+
+tbl <- list.files(pattern = value) %>% 
+  map_df(~read_plus(.))
+
+
+
+all_tbl <- list.files() %>% 
+  map_df(~read_plus(.))
+
+read_plus <- function(flnm) {
+  read_csv(flnm) %>% 
+    mutate(filename = flnm)
 }
+
+p <- all_tbl %>%
+  filter(genz == max(genz)) 
+
+y <- all_tbl %>% 
+  pivot_wider(
+    names_from = filename,
+    values_from = freqs) %>%
+  select(contains("d_0.8"))
+
+colors <- c(rep("#e25dbb", 5), 
+            rep("#6cdf4c", 5),
+            rep("#258bd2", 5))
+
+
+# plot the density distribution
+ggplot(p, aes(x = freqs, fill = filename)) +
+  geom_density(alpha = 0.2) + 
+  scale_fill_manual(values = colors,
+                    breaks = unique(all_tbl$filename)[c(1,6,11)],
+                    name = "Dominance",
+                    labels = c("0.2", "0.5", "0.8")) +
+  ylab("Density") + 
+  xlab("Allele Frequency") +
+  theme_classic()
+  
+
+x <- shapiro.test(p$freqs)
+
+x$p.value
+
+# so load all five reps, label them with another column and then group them and stack on top of eachother
